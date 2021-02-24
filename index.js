763cd0d739f577402e8f884355e3eabce09fd3d5
Anna <adropofwater0@gmail.com>
2021-02-24 21:05:12 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
 const readline = require('readline-sync')
-let operator = "";
-let operands = "";
-let arr = [];
 
-const printWelcomeMessage = function() {
-  console.log('Welcome to the calculator!')
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
+printWelcomeMessage();
+while (true) {
+  console.log("\nWhich calculator mode do you want?");
+  console.log("1) Arithmetic");
+  console.log("2) Vowel counting")
+
+  let calculationMode = getCalculationMode();
+  if (calculationMode === ARITHMETIC_MODE) {
+    performArithmeticCalculation();
+  } else if (calculationMode === VOWEL_COUNTING_MODE) {
+    performvoVowelCountingCalculation();
+  }
+}
+
+function printWelcomeMessage() {
+  console.log('\nWelcome to the calculator!')
   console.log('==========================')
-};
-const askForOperator = function() {
+}
+function askForOperator() {
   console.log('Please enter the operator: ');
-  operator = readline.prompt();
+  return readline.prompt();
 }
 
-const askForNumOfOperands = function() {
+function askForNumOfOperands(operator) {
   console.log(`How many numbers do you want to ${operator} ? `);
-  operands = readline.prompt();
-}
+  return readline.prompt();
 
-const createNumberArray = function() {
+};
+
+function createNumberArray(operands) {
+  let arr = [];
   for (let i = 1; i <= +operands; i++) {
-    console.log(` Please enter number ${i}:`);
+    console.log(` Please enter number ${i}:`); 
     let num = readline.prompt();
     if(isNaN(+num)) {
       console.log( `Error, ${num} is not a number!`);
       return;
     }
     arr.push(+num);
+  }return arr;
+}
+
+// function reduce(arr, n, reducer) {
+//   let accumulator = n;
+//   for (const element of arr) {
+//     accumulator = reducer(accumulator, element);
+//   } return accumulator;
+//}
+
+
+function reducerForOperator(operator) {
+  switch(operator) {
+    case '+' : return (a, b) => a + b;
+    
+    case '*' : return (a, b) => a * b;
+    
+    case '-' : return (a, b) => a - b;
+   
+    case '/' : return (a, b) => a / b;
+    
   }
+
 }
-const performOperation = function() {
-  let result = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    switch(operator) {
-      case '+' : result += arr[i];
-      break;
-      case '*' : result *= arr[i];
-      break;
-      case '-' : result -= arr[i];
-      break;
-      case '/' : result /= arr[i];
-      break;
-
-    }
-
-  } console.log(`The result is: ${result}`);
+function performOperation(operator, arr) {
+   const reducer = reducerForOperator(operator);
+  //  const n = arr[0];
+  //  const tail = arr.slice(1);
+   const result = arr.slice(1).reduce(reducer, arr[0]);
+   console.log(`The result is: ${result}`);
 
 }
 
-const performvoVowelCountingCalculation = function() {
+function performvoVowelCountingCalculation() {
   console.log("Please enter a string:");
   let str = readline.prompt();
   console.log("The vowel counts are:");
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
   for (let element of vowels) {
     let counter = 0;
     let vowel = element.toUpperCase();
@@ -65,30 +92,16 @@ const performvoVowelCountingCalculation = function() {
   }
 
 }
-const performArithmeticCalculation = function() {
-  askForOperator();
-  askForNumOfOperands();
-  createNumberArray();
-  performOperation();
+function performArithmeticCalculation() { 
+  const operator = askForOperator();
+  const operands = askForNumOfOperands(operator);
+  const arr = createNumberArray(operands);
+  performOperation(operator, arr);
+  console.log(arr);
 
 }
-const getCalculationMode = function() {
+function getCalculationMode() {
   let mode = readline.prompt();
   return mode;
 }
 
-const ARITHMETIC_MODE = '1';
-const VOWEL_COUNTING_MODE = '2';
-printWelcomeMessage();
-while (true) {
-  console.log("Which calculator mode do you want?");
-  console.log("1) Arithmetic");
-  console.log("2) Vowel counting")
-
-  let calculationMode = getCalculationMode();
-  if (calculationMode === ARITHMETIC_MODE) {
-    performArithmeticCalculation();
-  } else if (calculationMode === VOWEL_COUNTING_MODE) {
-    performvoVowelCountingCalculation();
-  }
-}
